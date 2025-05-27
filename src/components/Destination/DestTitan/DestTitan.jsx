812a0cfc6ac62e7e681ffefe7a276b1fc24f36bd
molengeek <saoussan.elk@gmail.com>
2025-05-27 16:40:50 +0200

    Eur+titan

--- src/components/Destination/DestTitan/DestTitan.jsx
+++ src/components/Destination/DestTitan/DestTitan.jsx
@@ -1,19 +1,19 @@
-import './DestEur.css'
-import Eur from '../../../assets/img/destination/image-europa.png'
+import './DestTitan.css'
+import Titan from '../../../assets/img/destination/image-Titan.png'
 import { NavLink } from 'react-router-dom'
 
 
-export default function DestEu(){
+export default function DestTitan(){
 
     return(
 
-        <div className='destEurGlo'>
-            <div className='divEurLeft'>
+        <div className='destTitanGlo'>
+            <div className='divTitanLeft'>
                 <div></div>
-                <div className='divEurImg'><img src={Eur} alt="" /></div>
+                <div className='divTitanImg'><img src={Titan} alt="" /></div>
             </div>
             <div></div>
-            <div className='divEurText'>
+            <div className='divTitanText'>
                 <div className='destNavList'>
                     <nav>
                         <NavLink to="/destination/moon" className='divDestNavLi'>MOON</NavLink>
@@ -22,8 +22,8 @@ export default function DestEu(){
                         <NavLink to="/destination/titan" className='divDestNavLi'>TITAN</NavLink>
                     </nav>
                 </div>
-                <h1>Eur</h1>
-                <p className='divEurTextP'> See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>  
+                <h1>Titan</h1>
+                <p className='divTitanTextP'> See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>  
                 <p>EST. TRAVEL TIME</p><h5>3 DAYS</h5>
             </div>
         </div>
