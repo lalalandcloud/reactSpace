c941a141370acfabe08a9ae196f6c993a9a024bb
molengeek <saoussan.elk@gmail.com>
2025-05-27 16:37:00 +0200

    Mars

--- src/components/Destination/DestMars/DestMars.jsx
+++ src/components/Destination/DestMars/DestMars.jsx
@@ -1,4 +1,6 @@
 import './DestMars.css'
+import Mars from '../../../assets/img/destination/image-Mars.png'
+import { NavLink } from 'react-router-dom'
 
 
 export default function DestMars(){
@@ -6,7 +8,24 @@ export default function DestMars(){
     return(
 
         <div className='destMarsGlo'>
-            <h1>ICI MARS</h1>
+            <div className='divMarsLeft'>
+                <div></div>
+                <div className='divMarsImg'><img src={Mars} alt="" /></div>
+            </div>
+            <div></div>
+            <div className='divMarsText'>
+                <div className='destNavList'>
+                    <nav>
+                        <NavLink to="/destination/moon" className='divDestNavLi'>MOON</NavLink>
+                        <NavLink to="/destination/mars" className='divDestNavLi'>MARS</NavLink>
+                        <NavLink to="/destination/eu" className='divDestNavLi'>EUROPA</NavLink>
+                        <NavLink to="/destination/titan" className='divDestNavLi'>TITAN</NavLink>
+                    </nav>
+                </div>
+                <h1>Mars</h1>
+                <p className='divMarsTextP'> See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>  
+                <p>EST. TRAVEL TIME</p><h5>3 DAYS</h5>
+            </div>
         </div>
     )
 }
